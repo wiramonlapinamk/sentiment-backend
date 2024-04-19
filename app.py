1cb40b0af4bac89503eb627c8dbe99f356a1bc1d
Wiramon Thongrubbai <wiramon.thongrubbai@stud.th-deg.de>
2024-04-19 16:06:10 +0300

    add post+get

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -6,5 +7,15 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, tWorld!</p>"
 
+@app.route("/wpwwwwww", methods=['GET','POST'])
+def hello_w():
+    if request.method =='POST':
+        payload = {'message':'post'}
+        print(payload)
+        return jsonify(payload)
+    payload = {'message':'get'}
+    print(payload)
+    return jsonify(payload)
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(port=8080)
