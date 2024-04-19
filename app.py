91aa2b3e2334b96cf59df59347a22f559df02a85
Wiramon Thongrubbai <wiramon.thongrubbai@stud.th-deg.de>
2024-04-19 20:29:19 +0300

    add python model

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 
+import pickle
+
+import pandas as pd
 from flask import Flask, jsonify, request
+from flask_cors import CORS  # CORS for handling Cross-Origin Resource Sharing
 
 app = Flask(__name__)
+CORS(app,resources={r"/*":{"origins":"*"}})
+
+
+model = pickle.load(open('ml_model.pkl', 'rb'))
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, tWorld!</p>"
+    return "<p>Hello, World!</p>"
 
-@app.route("/wpwwwwww", methods=['GET','POST'])
-def hello_w():
-    if request.method =='POST':
-        payload = {'message':'post'}
-        print(payload)
-        return jsonify(payload)
-    payload = {'message':'get'}
-    print(payload)
-    return jsonify(payload)
+@app.route("/predict", methods=['POST'])
+def predict():
+    try:
+        data = request.get_data()
+        query_df = ([data])
+        prediction = model.predict(query_df)
+        print(prediction)
+        return jsonify({'message' : list(prediction)})
+    except Exception as e:
+        return jsonify({'error':str(e)})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080)
