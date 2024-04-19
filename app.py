152c4bb6a9d906fbc571293e259dea0fd786bbf2
Wiramon Thongrubbai <wiramon.thongrubbai@stud.th-deg.de>
2024-04-19 16:13:07 +0300

    uncomment payload

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, tWorld!</p>"
 
-# @app.route("/wpwwwwww", methods=['GET','POST'])
-# def hello_w():
-#     if request.method =='POST':
-#         payload = {'message':'post'}
-#         print(payload)
-#         return jsonify(payload)
-#     payload = {'message':'get'}
-#     print(payload)
-#     return jsonify(payload)
+@app.route("/wpwwwwww", methods=['GET','POST'])
+def hello_w():
+    if request.method =='POST':
+        payload = {'message':'post'}
+        print(payload)
+        return jsonify(payload)
+    payload = {'message':'get'}
+    print(payload)
+    return jsonify(payload)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080)
